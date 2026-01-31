bc4c623c3afd1218eb69269828d51f80d8226dd1
Leonhard Hoffmann <Leonhard.hoffmann8@gmail.com>
2026-01-31 18:04:36 +0100

    feat: add basic content and styling

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,5 +1,20 @@
+import '../App.css';
+
 function App() {
-  return <></>;
+  return (
+    <>
+      <div className="app">
+        <div className="headingDiv">
+          <h1>React Starter – Enterprise Ready</h1>
+          <h3>A scalable, open-source React boilerplate for real-world apps</h3>
+        </div>
+        <div className="buttonDiv">
+          <button id="ghBtn">GitHub</button>
+          <button id="docBtn">Documentation</button>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default App;
